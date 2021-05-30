2f3633deffd240eb34bbf8fe93f14673b780209e
Robin Wood <robin@digi.ninja>
2021-05-30 16:00:58 +0100

    basics working

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"sort"
 	"strings"
 
 	"github.com/dghubble/go-twitter/twitter"
@@ -17,20 +18,22 @@ func main() {
 	//fmt.Println("Starting")
 
 	flags := struct {
-		consumerKey    string
-		consumerSecret string
-		term           string
-		records        int
+		apiKey     string
+		apiSecret  string
+		term       string
+		records    int
+		min_length int
 	}{}
 
-	flag.StringVar(&flags.consumerKey, "consumer-key", "", "Twitter Consumer Key")
-	flag.StringVar(&flags.consumerSecret, "consumer-secret", "", "Twitter Consumer Secret")
+	flag.StringVar(&flags.apiKey, "api-key", "", "Twitter API Key")
+	flag.StringVar(&flags.apiSecret, "api-secret", "", "Twitter API Secret")
 	flag.StringVar(&flags.term, "term", "", "Search Term")
 	flag.IntVar(&flags.records, "records", 5, "Number of search results")
+	flag.IntVar(&flags.min_length, "min_length", 5, "Minimum word length")
 	flag.Parse()
 	//flagutil.SetFlagsFromEnv(flag.CommandLine, "TWITTER")
 
-	if flags.consumerKey == "" || flags.consumerSecret == "" {
+	if flags.apiKey == "" || flags.apiSecret == "" {
 		log.Fatal("Application Access Token required")
 	}
 	if flags.term == "" {
@@ -39,8 +42,8 @@ func main() {
 
 	// oauth2 configures a client that uses app credentials to keep a fresh token
 	config := &clientcredentials.Config{
-		ClientID:     flags.consumerKey,
-		ClientSecret: flags.consumerSecret,
+		ClientID:     flags.apiKey,
+		ClientSecret: flags.apiSecret,
 		TokenURL:     "https://api.twitter.com/oauth2/token",
 	}
 	// http.Client will automatically authorize Requests
@@ -64,27 +67,57 @@ func main() {
 
 	var re = regexp.MustCompile(`[^\w \s \d]`)
 
+	data := make(map[string]int)
+
 	for _, res := range search.Statuses {
-		fmt.Printf("XXXXXXXXXXXXXXXX\n")
-		fmt.Printf("%s\n", res.Text)
+		//fmt.Printf("XXXXXXXXXXXXXXXX\n")
+		//	fmt.Printf("%s\n", res.Text)
 		clean := re.ReplaceAllString(res.Text, ` `)
-		fmt.Printf("%s\n", clean)
+		//	fmt.Printf("%s\n", clean)
 		words := strings.Fields(clean)
-		for i, word := range words {
-			fmt.Printf("%d, %s\n", i, word)
+		for _, word := range words {
+			if len(word) >= flags.min_length {
+				if _, ok := data[word]; !ok {
+					data[word] = 0
+				}
+				data[word]++
+
+				//		fmt.Printf("%d, %s\n", i, word)
+			}
 		}
-
-		// fmt.Printf("%+v\n", res)
 	}
-	//fmt.Printf("SEARCH TWEETS:\n%+v\n", search)
-	//fmt.Printf("SEARCH METADATA:\n%+v\n", search.Metadata)
 
-	// Search Tweets
 	/*
-		search, resp, err := client.Search.Tweets(&twitter.SearchTweetParams{
-			Query: "gopher",
-		})
-		fmt.Printf("search: %s\n", search)
-		fmt.Println("Done")
+		To sort the results alphabetically
+		keys := make([]string, 0, len(data))
+		for k := range data {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
 	*/
+
+	type kv struct {
+		Key   string
+		Value int
+	}
+
+	var ss []kv
+	for k, v := range data {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	for _, kv := range ss {
+		fmt.Printf("%s, %d\n", kv.Key, kv.Value)
+	}
+
+	/*
+		for _, k := range keys {
+			fmt.Println(k, data[k])
+		}
+	*/
+
 }
