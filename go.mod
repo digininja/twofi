3514fbe28577c66dbb2036f0a2152605862d5d74
Robin Wood <robin@digi.ninja>
2021-06-01 19:49:49 +0100

    nearly ready to launch

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"regexp"
 	"sort"
 	"strings"
@@ -23,13 +23,19 @@ func main() {
 		term       string
 		records    int
 		min_length int
+		inc_counts bool
+		inc_tweets bool
+		debug      bool
 	}{}
 
 	flag.StringVar(&flags.apiKey, "api-key", "", "Twitter API Key")
 	flag.StringVar(&flags.apiSecret, "api-secret", "", "Twitter API Secret")
-	flag.StringVar(&flags.term, "term", "", "Search Term")
+	flag.StringVar(&flags.term, "term", "", "Search Term - For tweets from a specific user prefix the user with 'from:'")
 	flag.IntVar(&flags.records, "records", 5, "Number of search results")
-	flag.IntVar(&flags.min_length, "min_length", 5, "Minimum word length")
+	flag.IntVar(&flags.min_length, "min-length", 5, "Minimum word length")
+	flag.BoolVar(&flags.inc_counts, "inc-counts", false, "Include counts")
+	flag.BoolVar(&flags.inc_tweets, "inc-tweets", false, "Include tweets")
+	flag.BoolVar(&flags.debug, "debug", false, "Debug")
 	flag.Parse()
 	//flagutil.SetFlagsFromEnv(flag.CommandLine, "TWITTER")
 
@@ -38,6 +44,10 @@ func main() {
 	}
 	if flags.term == "" {
 		log.Fatal("No search term provided")
+	}
+
+	if flags.debug {
+		log.SetLevel(log.DebugLevel)
 	}
 
 	// oauth2 configures a client that uses app credentials to keep a fresh token
@@ -57,23 +67,31 @@ func main() {
 	// https://github.com/dghubble/go-twitter/blob/master/twitter/search.go
 	searchTweetParams := &twitter.SearchTweetParams{
 		Query: flags.term,
-		// TweetMode: "extended",
+		// TweetMode: "extended", - When used, the result is in FullText not in Text
 		Count: flags.records,
 	}
 
 	search, _, _ := client.Search.Tweets(searchTweetParams)
 	// Print all
-	// fmt.Printf("%+v\n", search.Statuses)
+	log.Debugf("%+v\n", search.Statuses)
 
 	var re = regexp.MustCompile(`[^\w \s \d]`)
 
+	// Structure of a tweet - called a status
+	// https://github.com/dghubble/go-twitter/blob/master/twitter/statuses.go
+	// Twitter API docs
+	// https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
 	data := make(map[string]int)
 
-	for _, res := range search.Statuses {
-		//fmt.Printf("XXXXXXXXXXXXXXXX\n")
-		//	fmt.Printf("%s\n", res.Text)
-		clean := re.ReplaceAllString(res.Text, ` `)
-		//	fmt.Printf("%s\n", clean)
+	for _, status := range search.Statuses {
+		// Dump the full response
+		log.Debugf("%+v\n", status)
+		if flags.inc_tweets {
+			fmt.Printf("%s\n", status.Text)
+			// fmt.Printf("%s\n", status.FullText)
+		}
+		clean := re.ReplaceAllString(status.Text, ` `)
+		log.Debugf("%s\n", clean)
 		words := strings.Fields(clean)
 		for _, word := range words {
 			if len(word) >= flags.min_length {
@@ -81,20 +99,9 @@ func main() {
 					data[word] = 0
 				}
 				data[word]++
-
-				//		fmt.Printf("%d, %s\n", i, word)
 			}
 		}
 	}
-
-	/*
-		To sort the results alphabetically
-		keys := make([]string, 0, len(data))
-		for k := range data {
-			keys = append(keys, k)
-		}
-		sort.Strings(keys)
-	*/
 
 	type kv struct {
 		Key   string
@@ -111,13 +118,10 @@ func main() {
 	})
 
 	for _, kv := range ss {
-		fmt.Printf("%s, %d\n", kv.Key, kv.Value)
-	}
-
-	/*
-		for _, k := range keys {
-			fmt.Println(k, data[k])
+		if flags.inc_counts {
+			fmt.Printf("%s, %d\n", kv.Key, kv.Value)
+		} else {
+			fmt.Printf("%s\n", kv.Key)
 		}
-	*/
-
+	}
 }
